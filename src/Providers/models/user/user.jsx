575fb48bc4f9fca0b774f45930ac7f3b67ae8ef0
Saamu192 <samuelpr192@gmail.com>
2022-07-06 12:45:05 -0300

    feat!:  provider callback

--- src/Providers/models/user/user.jsx
+++ src/Providers/models/user/user.jsx
@@ -6,32 +6,43 @@ export const UserContext = createContext([]);
 
 export function UserProvider({ children }) {
   const [user, setUser] = useState(null);
+  const [register, setRegister] = useState(false);
+  const [login, setLogin] = useState(false);
 
-  async function createUser(email, password, name) {
+  async function createUser(email, password, name, callback) {
     const data = { email, password, name };
 
     const res = Api.post("/register", data)
       .then((response) => {
         console.log(response.data);
+        setRegister(true);
       })
       .catch((err) => {
         console.log(err);
       });
+    if (callback) {
+      callback(register);
+    }
     return res;
   }
 
-  async function loginUser(email, password) {
+  async function loginUser(email, password, callback) {
     const data = { email, password };
 
     const res = await Api.post("/login", data)
       .then((response) => {
         setUser(response.data.user);
+        setLogin(true);
 
         localStorage.setItem(
           "@Easy:Token",
           JSON.stringify(response.data.accessToken)
         );
         localStorage.setItem("@Easy:Id", JSON.stringify(response.data.user.id));
+
+        if (callback) {
+          callback(login);
+        }
       })
       .catch((err) => {
         console.log(err);
