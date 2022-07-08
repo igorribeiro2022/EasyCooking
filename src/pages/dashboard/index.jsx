63e9c3876857bd34e65d10f57f694adb100b0ebb
Saamu192 <samuelpr192@gmail.com>
2022-07-08 11:05:56 -0300

    feat!: verify user

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,26 +1,28 @@
-import {Header} from '../../components/Templates/Header/index.jsx';
-import {Container} from './style.js'
-import {UserRecipes} from '../../components/Templates/UserRecipes';
+import { Header } from "../../components/Templates/Header/index.jsx";
+import { Container } from "./style.js";
+import { UserRecipes } from "../../components/Templates/UserRecipes";
+import { UserContext } from "../../Providers/models/user/user.jsx";
+import { useContext } from "react";
 
-function DashBoard () {
-
+function DashBoard() {
+  const { verify } = useContext(UserContext);
 
   return (
     <>
-     <Container>
-      <Header/>
-      <div className="buttonsDiv">
-        <button className="button">Minhas Receitas</button> 
-        <button className="button">Receitas Salvas</button> 
-        <button className="button">Ingredientes</button> 
-      </div>
+      <Container>
+        <Header />
+        <div className="buttonsDiv">
+          <button className="button">Minhas Receitas</button>
+          <button className="button">Receitas Salvas</button>
+          <button className="button">Ingredientes</button>
+        </div>
 
-      <div className="dashboardContent">
-        <UserRecipes/>
-      </div>
+        <div className="dashboardContent">
+          <UserRecipes />
+        </div>
       </Container>
     </>
-  )
+  );
 }
 
 export default DashBoard;
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -5,7 +5,8 @@ import { GiKnifeFork } from "react-icons/gi";
 import { TbCookieOff } from "react-icons/tb";
 import { TbCookie } from "react-icons/tb";
 import loginImg from "../../assets/LoginImg.png";
-import LogoV2Black from "../../assets/LogoV2-Black.png";import { Button } from "../../components/Atoms/Button";
+import LogoV2Black from "../../assets/LogoV2-Black.png";
+import { Button } from "../../components/Atoms/Button";
 import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -13,16 +14,15 @@ import * as yup from "yup";
 import { UserContext } from "../../Providers/models/user/user";
 import { useNavigate } from "react-router-dom";
 
-
 export default function LoginPage() {
   const [inputType, setInputType] = useState("password");
   const { loginUser } = useContext(UserContext);
-  const navigate = useNavigate()
+  const { verify } = useContext(UserContext);
+  const navigate = useNavigate();
 
   const callBackNavigate = (page) => {
-    navigate(page)
-  }
-  
+    navigate(page);
+  };
 
   const formSchema = yup.object().shape({
     email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
@@ -37,16 +37,15 @@ export default function LoginPage() {
     resolver: yupResolver(formSchema),
   });
 
-  function onSubmitFunction({ email, password}) {
-    loginUser(email, password, () => callBackNavigate('/'))   
+  function onSubmitFunction({ email, password }) {
+    loginUser(email, password, () => callBackNavigate("/"));
   }
 
   return (
     <Page>
-
       <div className="mainDiv">
         <figure>
-          <img src={LogoV2Black} alt="Easy Cooking"/>
+          <img src={LogoV2Black} alt="Easy Cooking" />
         </figure>
         <Form onSubmit={handleSubmit(onSubmitFunction)}>
           <h1>Faça seu login</h1>
@@ -65,7 +64,8 @@ export default function LoginPage() {
           </div>
           <div>
             <label>
-              Senha {errors.password && <span> - {errors.password.message}</span>}
+              Senha{" "}
+              {errors.password && <span> - {errors.password.message}</span>}
             </label>
             <div>
               <GiKnifeFork />
@@ -88,13 +88,24 @@ export default function LoginPage() {
             </div>
           </div>
           <Button type="submit">Login</Button>
-          <h4>Ainda não tem cadastro? Cadasatre-se <span className="link" onClick={() => callBackNavigate('/cadastro')}>aqui</span></h4>
+          <h4>
+            Ainda não tem cadastro? Cadasatre-se{" "}
+            <span
+              className="link"
+              onClick={() => callBackNavigate("/cadastro")}
+            >
+              aqui
+            </span>
+          </h4>
         </Form>
-    
       </div>
 
-      <img className="pic" src={loginImg} alt="Easy Cooking"  onClick={() => callBackNavigate('/')}/>
-
+      <img
+        className="pic"
+        src={loginImg}
+        alt="Easy Cooking"
+        onClick={() => callBackNavigate("/")}
+      />
     </Page>
   );
 }
