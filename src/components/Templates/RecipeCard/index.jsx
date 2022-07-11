81c6efb54e9f25af871aa62920068a4aa02f6631
igorribeiro2022 <igor.rogerio.ribeiro@gmail.com>
2022-07-11 11:50:14 -0300

    refactor: style home

--- src/components/Templates/RecipeCard/index.jsx
+++ src/components/Templates/RecipeCard/index.jsx
@@ -1,21 +1,29 @@
 import {RecipeCardContainer} from './style.js'
 import {FullRecipe} from '../../Templates/FullRecipe';
 import {useState} from 'react';
+import { Rating } from '@mui/material';
 
 function RecipeCard ({recipe,key}) {
   const [isOpen, setIsOpen] = useState(false);
+  const [rating, setRating] = useState(2)
+  const token = localStorage.getItem("@Easy:Token")
+  const ratingAPI = recipe.rating
 
-    
   return (
     <>
     <RecipeCardContainer>
-    <div className="RecipeCard">
-      <div className='ImgDiv'>
+      <figure className='imgFig'>
         <img src={recipe.image} alt=""/>
-      </div>
+      </figure>
       <h1 className="RecipeName">{recipe.name}</h1>
-      <button  onClick={() => setIsOpen(true)} className="RecipeButton">View Recipe</button>
-    </div>
+      <div>
+        {
+        token !== null ? <Rating defaultValue={ratingAPI} value={rating} onChange={(e) => setRating(e.target.value)} size="small" /> 
+        : 
+        <Rating defaultValue={ratingAPI} value={rating} size="small" readOnly/> 
+        }
+        <button  onClick={() => setIsOpen(true)} className="RecipeButton">Ver Receita</button>
+      </div>
     </RecipeCardContainer>
     {/* <FullRecipe state={isOpen} setState={setIsOpen}/> */}
     </>
