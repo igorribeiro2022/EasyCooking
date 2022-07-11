6190d087222edbc7be080d07c0a5b4602b6b2c11
Saamu192 <samuelpr192@gmail.com>
2022-07-11 08:22:03 -0300

    feat: verify on dashboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -2,26 +2,32 @@ import { Header } from "../../components/Templates/Header/index.jsx";
 import { Container } from "./style.js";
 import { UserRecipes } from "../../components/Templates/UserRecipes";
 import { UserContext } from "../../Providers/models/user/user.jsx";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function DashBoard() {
   const { verify } = useContext(UserContext);
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (!verify) {
+      return navigate("/login");
+    }
+  }, [verify]);
 
   return (
-    <>
-      <Container>
-        <Header />
-        <div className="buttonsDiv">
-          <button className="button">Minhas Receitas</button>
-          <button className="button">Receitas Salvas</button>
-          <button className="button">Ingredientes</button>
-        </div>
+    <Container>
+      <Header />
+      <div className="buttonsDiv">
+        <button className="button">Minhas Receitas</button>
+        <button className="button">Receitas Salvas</button>
+        <button className="button">Ingredientes</button>
+      </div>
 
-        <div className="dashboardContent">
-          <UserRecipes />
-        </div>
-      </Container>
-    </>
+      <div className="dashboardContent">
+        <UserRecipes />
+      </div>
+    </Container>
   );
 }
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,20 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
-import LandingPage from '../pages/landingPage'
-import LoginPage from '../pages/login'
-import RegisterPage from '../pages/register'
-import Home from '../pages/home'
-import DashBoard from '../pages/dashboard'
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "../pages/landingPage";
+import LoginPage from "../pages/login";
+import RegisterPage from "../pages/register";
+import Home from "../pages/home";
+import DashBoard from "../pages/dashboard";
 
-
-export default function RoutesApp (){
-    return(
-        <Routes>
-            
-            <Route path="/landing" element={<LandingPage/>}/>
-            <Route path="/login" element={<LoginPage/>}/>
-            <Route path="/cadastro" element={<RegisterPage/>}/>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/dashboard" element={<DashBoard/>}/>
-        </Routes>
-    )
+export default function RoutesApp() {
+  return (
+    <Routes>
+      <Route path="/landing" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/cadastro" element={<RegisterPage />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/dashboard" element={<DashBoard />} />
+    </Routes>
+  );
 }
