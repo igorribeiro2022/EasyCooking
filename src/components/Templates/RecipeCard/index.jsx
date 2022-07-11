f1cac1b1252bb9959a547211753b814aecaf5f31
Saamu192 <samuelpr192@gmail.com>
2022-07-11 15:52:27 -0300

    fix: button modal as been deleted

--- src/components/Templates/RecipeCard/index.jsx
+++ src/components/Templates/RecipeCard/index.jsx
@@ -1,40 +1,52 @@
-import {RecipeCardContainer} from './style.js'
-import { useNavigate, useParams } from 'react-router-dom';
-import { Rating } from '@mui/material';
-import {useState} from 'react';
+import { RecipeCardContainer } from "./style.js";
+import { useNavigate, useParams } from "react-router-dom";
+import { Rating } from "@mui/material";
+import { useState } from "react";
 
-function RecipeCard ({recipe}) {
-  const navigate = useNavigate()
-  const { id } = useParams()
-  const [rating, setRating] = useState(2)
-  const token = localStorage.getItem("@Easy:Token")
-  const ratingAPI = recipe.rating
+function RecipeCard({ recipe }) {
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+  const [rating, setRating] = useState(2);
+  const token = localStorage.getItem("@Easy:Token");
+  const ratingAPI = recipe.rating;
 
   const handleView = () => {
-    navigate(`receita/${recipe.name}`)
-  }
+    navigate(`receita/${recipe.name}`);
+  };
   return (
     <>
-    <RecipeCardContainer>
-      <figure className='imgFig'>
-        <img src={recipe.image} alt=""/>
-      </figure>
-      <h1 className="RecipeName">{recipe.name}</h1>
+      <RecipeCardContainer>
+        <figure className="imgFig">
+          <img src={recipe.image} alt="" />
+        </figure>
+        <h1 className="RecipeName">{recipe.name}</h1>
 
-      <button  onClick={() => handleView()} className="RecipeButton">Ver receita</button>
-    </div>
-      <div>
-        {
-        token !== null ? <Rating defaultValue={ratingAPI} value={rating} onChange={(e) => setRating(e.target.value)} size="small" /> 
-        : 
-        <Rating defaultValue={ratingAPI} value={rating} size="small" readOnly/> 
-        }
-        <button  onClick={() => setIsOpen(true)} className="RecipeButton">Ver Receita</button>
-      </div>
-    </RecipeCardContainer>
-    {/* <FullRecipe state={isOpen} setState={setIsOpen}/> */}
+        <button onClick={() => handleView()} className="RecipeButton">
+          Ver receita
+        </button>
+
+        <div>
+          {token !== null ? (
+            <Rating
+              defaultValue={ratingAPI}
+              value={rating}
+              onChange={(e) => setRating(e.target.value)}
+              size="small"
+            />
+          ) : (
+            <Rating
+              defaultValue={ratingAPI}
+              value={rating}
+              size="small"
+              readOnly
+            />
+          )}
+        </div>
+      </RecipeCardContainer>
+      {/* <FullRecipe state={isOpen} setState={setIsOpen}/> */}
     </>
-  )
+  );
 }
 
 export default RecipeCard;
