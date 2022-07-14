62fb4bd1cd3067e2466fc79d04313889c66734b6
Saamu192 <samuelpr192@gmail.com>
2022-07-13 21:44:57 -0300

    fix: rating change

--- src/components/Templates/RecipeCard/index.jsx
+++ src/components/Templates/RecipeCard/index.jsx
@@ -1,14 +1,19 @@
-import { RecipeCardContainer, Span, StyledButton, StyleRating, } from "./style.js";
+import {
+  RecipeCardContainer,
+  Span,
+  StyledButton,
+  StyleRating,
+} from "./style.js";
 import { useNavigate } from "react-router-dom";
 import { Rating } from "@mui/material";
-import { useContext, useEffect, useState } from "react";
-import { UserContext } from "../../../Providers/models/user/user.jsx";
+import { useEffect, useState } from "react";
+
 import { Api } from "../../../services/api.js";
 
 function RecipeCard({ recipe, del, setMyRecipes, myRecipes }) {
   const navigate = useNavigate();
-  const { verify } = useContext(UserContext);
   const [rating, setRating] = useState(2);
+  const [dataUser, setDataUser] = useState(null);
 
   useEffect(() => {
     const value = recipe.reviews?.reduce((prev, acc) => prev + acc.rating, 0);
@@ -41,25 +46,26 @@ function RecipeCard({ recipe, del, setMyRecipes, myRecipes }) {
           !del && handleView();
         }}
       >
-        <figure className="imgFig">
+        <figure
+          onClick={() => {
+            del && handleView();
+          }}
+          className="imgFig"
+        >
           <img src={recipe.image} alt="" />
         </figure>
 
         <p className="RecipeName">{recipe.name}</p>
 
         {del && (
-          <span className="delBtn" onClick={() => handleDelete()}>Delete</span>
+          <span className="delBtn" onClick={() => handleDelete()}>
+            Delete
+          </span>
         )}
         <div>
-          {verify ? (
-            <StyleRating>
-              <Rating value={rating} size="small" readOnly />
-            </StyleRating>
-          ) : (
-            <StyleRating>
-              <Rating value={rating} size="small" readOnly />
-            </StyleRating>
-          )}
+          <StyleRating>
+            <Rating value={rating} size="small" readOnly />
+          </StyleRating>
 
           <Span className="RecipeButton" lunch={recipe.category}>
             {recipe.category}
--- src/pages/recipePage/index.jsx
+++ src/pages/recipePage/index.jsx
@@ -4,7 +4,13 @@ import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { Header } from "../../components/Templates/Header";
 import { RecipesContext } from "../../Providers/models/recipes/recipes";
-import { ContentPage, NameRecipe, Preparation, RatingStyle, StyleContainer } from "./style";
+import {
+  ContentPage,
+  NameRecipe,
+  Preparation,
+  RatingStyle,
+  StyleContainer,
+} from "./style";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import { BsFillSaveFill } from "react-icons/bs";
 import { Rating } from "@mui/material";
@@ -14,99 +20,126 @@ import { IngredientsContext } from "../../Providers/models/ingredients/ingredien
 import { Api } from "../../services/api";
 
 function RecipePage() {
-    const { recipeName } = useParams();
-    const { ratingMax } = useContext(IngredientsContext);
+  const { recipeName } = useParams();
+  const { ratingMax } = useContext(IngredientsContext);
 
-    const { saveRecipe } = useContext(UserContext);
-    const { user } = useContext(UserContext);
-    const [rating, setRating] = useState(null);
-    const [onlyRecipe, setonlyRecipe] = useState(null);
+  const { saveRecipe } = useContext(UserContext);
+  const [rating, setRating] = useState(null);
+  const [onlyRecipe, setonlyRecipe] = useState(null);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        Api.get(`/recipes/${recipeName}`)
-            .then((res) => { setonlyRecipe(res.data); })
-            .catch((err) => console.log(err));
+  useEffect(() => {
+    Api.get(`/recipes/${recipeName}`)
+      .then((res) => {
+        setonlyRecipe(res.data);
+      })
+      .catch((err) => console.log(err));
 
-        const value = onlyRecipe?.reviews?.reduce((prev, acc) => prev + acc.rating, 0);
-        const result = value / onlyRecipe?.reviews.length;
-        setRating(result);
-    }, []);
-
-    const handleBack = () => {
-        window.history.back();
-    };
-
-    const handleRating = (e) => {
-        ratingMax(user, onlyRecipe, e);
-    };
-
-    const handleSave = () => {
-        const data = user.favorites;
-        
-        if(data.map ((recipe) => recipe.id).includes(onlyRecipe.id)) {
-
-            console.log("receita já salva");
-        }
-        else {           
-            data.push(onlyRecipe);
-            return saveRecipe(data);
-        }
-    };
-    console.log(user.favorites);
-
-
-    return (
-        <>
-            <Header />
-            <NameRecipe>
-                <div className="titleAndRating">
-                <h1>{onlyRecipe?.name}</h1>
-                        {user ? (
-                            <RatingStyle>
-                                <Rating value={rating} onChange={(event, newValue) => { handleRating(newValue); }} />
-                            </RatingStyle>
-                        ) : (
-                            <RatingStyle>
-                                <Rating readOnly value={rating} />
-                            </RatingStyle>
-                        )}
-                </div>
-
-                <div className="divBtn">
-                    <button onClick={() => { handleBack(); }}>
-                        <AiOutlineArrowLeft /> Voltar
-                    </button>
-                    <button onClick={() => { handleSave(); }}>
-                        Salvar <BsFillSaveFill />
-                    </button>
-                </div>
-            </NameRecipe>
-            <StyleContainer>
-                <ContentPage>
-                    <div className="divIngredients">
-                        <h2>Ingredientes</h2>
-                        <ul>
-                            {onlyRecipe?.ingredients.map((ingredient, index) => (
-                                <li key={index}>
-                                    {ingredient.name} - {ingredient.quantity} {ingredient.unit}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-
-                    <div className="divImage">
-                        <figure>
-                            <img src={onlyRecipe?.image} alt={onlyRecipe?.name} />
-                        </figure>
-                    </div>
-                </ContentPage>
-                <Preparation>
-                    <h2>Modo de preparo</h2>
-                    <p>{onlyRecipe?.preparation_mode}</p>
-                </Preparation>
-            </StyleContainer>
-        </>
+    const value = onlyRecipe?.reviews?.reduce(
+      (prev, acc) => prev + acc.rating,
+      0
     );
+    const result = value / onlyRecipe?.reviews.length;
+    setRating(result);
+  }, []);
+
+  useEffect(() => {
+    const token = localStorage.getItem("@Easy:Token");
+    const id = localStorage.getItem("@Easy:Id");
+    Api.get(`/users/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => setUser(res.data))
+      .catch((err) => console.log(err));
+  });
+
+  const handleBack = () => {
+    window.history.back();
+  };
+
+  const handleRating = (e) => {
+    ratingMax(user, onlyRecipe, e);
+    setRating(e);
+  };
+
+  const handleSave = () => {
+    const data = user.favorites;
+
+    if (data.map((recipe) => recipe.id).includes(onlyRecipe.id)) {
+      console.log("receita já salva");
+    } else {
+      data.push(onlyRecipe);
+      return saveRecipe(data);
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      <NameRecipe>
+        <div className="titleAndRating">
+          <h1>{onlyRecipe?.name}</h1>
+          {user ? (
+            <RatingStyle>
+              <Rating
+                value={rating}
+                onChange={(event, newValue) => {
+                  handleRating(newValue);
+                }}
+              />
+            </RatingStyle>
+          ) : (
+            <RatingStyle>
+              <Rating readOnly value={rating} />
+            </RatingStyle>
+          )}
+        </div>
+
+        <div className="divBtn">
+          <button
+            onClick={() => {
+              handleBack();
+            }}
+          >
+            <AiOutlineArrowLeft /> Voltar
+          </button>
+          <button
+            onClick={() => {
+              handleSave();
+            }}
+          >
+            Salvar <BsFillSaveFill />
+          </button>
+        </div>
+      </NameRecipe>
+      <StyleContainer>
+        <ContentPage>
+          <div className="divIngredients">
+            <h2>Ingredientes</h2>
+            <ul>
+              {onlyRecipe?.ingredients.map((ingredient, index) => (
+                <li key={index}>
+                  {ingredient.name} - {ingredient.quantity} {ingredient.unit}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="divImage">
+            <figure>
+              <img src={onlyRecipe?.image} alt={onlyRecipe?.name} />
+            </figure>
+          </div>
+        </ContentPage>
+        <Preparation>
+          <h2>Modo de preparo</h2>
+          <p>{onlyRecipe?.preparation_mode}</p>
+        </Preparation>
+      </StyleContainer>
+    </>
+  );
 }
 
 export default RecipePage;
